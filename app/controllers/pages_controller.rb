fcac432e943e98821e5fe02d5a834eaf9980916c
charlotte bunter <charlottebunter@gmail.com>
2025-12-02 15:54:49 +0100

    user dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @events = Event.limit(5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  root "pages#home"
 
+  # Home page
+  root "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
 end
