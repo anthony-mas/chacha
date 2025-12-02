811cb8f6d3ffeff300e2c77e24327f42e41123c1
Josephine Katz-Trataris <josephine.katztrataris@gmail.com>
2025-12-02 17:29:23 +0100

    routes nested resources

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -2,10 +2,7 @@ class ReactionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:create]
 
-  # POST /posts/:post_id/reactions
   def create
-    # Reactions are simple: a user either has one or doesn't.
-    # This creates the association (liking the post).
     @reaction = @post.reactions.new(user: current_user)
 
     if @reaction.save
@@ -15,9 +12,7 @@ class ReactionsController < ApplicationController
     end
   end
 
-  # DELETE /posts/:post_id/reactions/:id (or find by user/post pair)
   def destroy
-    # Assuming we are deleting the current user's reaction for the post
     @reaction = Reaction.find_by!(user: current_user, post_id: params[:post_id])
 
     if @reaction.destroy
@@ -32,6 +27,4 @@ class ReactionsController < ApplicationController
   def set_post
     @post = Post.find(params[:post_id])
   end
-
-  # No strong params needed as reactions simply link a user to a post
 end
