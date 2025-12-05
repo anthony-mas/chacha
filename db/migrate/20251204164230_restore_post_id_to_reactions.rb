aada2af1310c6c571595e6da1a4879c136d93811
Josephine Katz-Trataris <josephine.katztrataris@gmail.com>
2025-12-05 11:03:23 +0100

    push some fixes

--- db/migrate/20251204164230_restore_post_id_to_reactions.rb
+++
@@ -1,9 +0,0 @@
-class RestorePostIdToReactions < ActiveRecord::Migration[7.1]
-  def change
-    remove_index :reactions, name: "index_reactions_on_reactable"
-    remove_column :reactions, :reactable_type
-    remove_column :reactions, :reactable_id
-
-    add_reference :reactions, :post, null: false, foreign_key: true
-  end
-end
